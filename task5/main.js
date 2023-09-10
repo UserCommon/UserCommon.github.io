e1e11e02948fdee783ac6522829da41e97ca9c00
UserCommon <kirilnenashev@gmail.com>
2023-09-10 12:28:30 +0300

    guess finished task5

---
+++ task5/main.js
@@ -0,0 +1,43 @@
+const prices = {
+    "Арбуз": 100,
+    "Дыня": 87,
+    "Банан": 70,
+    "Яблоко": 30
+}
+
+let setCalculation = (value) => {
+    let domAns = document.getElementById("result")
+    if(validateInput(value)) {
+        domAns.replaceChildren(`Стоимость: ${value}`); 
+    } else {
+        domAns.replaceChildren(`Недопустимое значение!`); 
+    }
+    
+}
+
+let getCalculation = () => {
+    let amount = document.getElementById("amount").value;
+    let select = document.getElementById("choice").value;
+
+    return prices[select] * Number(amount);
+}
+
+let onSubmit = () => {
+    let val = getCalculation();
+    setCalculation(val);
+}
+
+let validateInput = (obj) => {
+    const regex = /^[0-9]+$/;
+
+    if(!regex.test(obj)) {
+        return false;
+    }
+    return true;
+} 
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM loaded");
+    let btn = document.getElementById("calculate");
+    btn.addEventListener('click', onSubmit);
+});
