4d6f66299c021214b373b7e1b6f31591414b4385
UserCommon <kirilnenashev@gmail.com>
2023-09-12 00:18:54 +0300

    changed 6 and 1

--- task6/service_calc.js
+++ task6/service_calc.js
@@ -1,14 +1,19 @@
 const food_prices = {
     "Бургер": 300,
     "Шаурма": 200,
-    "Картофель фри": 100
+    "Картофель фри": 100,
 };
 
 const additional_prices = {
-    "Посыпать солью": 20,
     "Сырный лаваш": 50,
     "Обычный лаваш": 0
 };
+
+const salt_price = {
+    true: 20,
+    false: 0
+};
+
 
 function calculate(food_type, additional_type, amount) {
     if(/^[0-9]+$/.test(amount))
@@ -20,47 +25,66 @@ function changeValue(value) {
     document.getElementById("result").replaceChildren(value);
 }
 
+function updatePrice() {
+    let VALUE = 0;
+    let radios = document.getElementsByName("r");
+    let radio_val = "Бургер";
+
+    radios.forEach((radio) => {
+        if(radio.checked) {
+            radio_val = radio.value;
+            let radio_price = food_prices[radio.value];
+
+            if (radio_price !== undefined) {
+                VALUE += radio_price;
+            }
+        }
+    });
+
+    let sel = document.getElementById("add");
+    sel.style.display = (radio_val == "Шаурма") ? "block" : "none";
+    let selection = document.getElementById("additional");
+    let select = selection[0];
+
+    VALUE += additional_prices[select.value];
+
+    let check = document.getElementById("check");
+    check.style.display = (radio_val == "Картофель фри" ? "block" : "none");
+
+    
+
+    console.log(select);
+    console.log(VALUE)
+}
 
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM loaded");
-    let calcuation = 0;
-
     let radios = document.getElementsByName("r");
-    let stateDom = document.getElementById("state");
-    let state = "Бургер";
-    let input = document.getElementById("amount_service");
-    let add = document.getElementById("add");
-    let additional = document.getElementById("additional")
+
+    let additionals = document.getElementById("add");
+    let additional_select = document.getElementById("additional");
     let check = document.getElementById("check");
 
-    radios.forEach((radio) => {
-        radio.addEventListener('change', (event) => {
-            state = radio.value;
+    additionals.style.display = "none";
+    check.style.display = "none";
 
-            calcuation = calculate(state, additional.value, input.value);
-            if(state == "Бургер") {
-                add.style.display = "none";
-                check.style.display = "none";
-            } else if (state == "Шаурма") {
-                console.log(state)
-                add.style.display = "block";
-                check.style.display = "none";
-            } else if (state == "Картофель фри") {
-                add.style.display = "none";
-                check.style.display = "block";
-            }
-            changeValue(calcuation);
+    additional_select.addEventListener("change", (event) => {
+        let target = event.target;
+        console.log(target.value);
+        updatePrice();
+    });
+
+    radios.forEach((radio) => {
+        radio.addEventListener("change", (event) => {
+            let target = event.target;
+            updatePrice();
         })
     });
 
-    additional.addEventListener('change', () => {
-        calcuation = calculate(state, additional.value, input.value);
-        changeValue(calcuation);
-    })
-
-    check.addEventListener('change', () => {
-        calcuation = calculate(state, additional.value, input.value);
-        changeValue(calcuation);
+    document.getElementById("checkbox").addEventListener("change", (event) => {
+        let target = event.target;
+        console.log(target.value)
+        updatePrice();
+   
     });
-    
 });
