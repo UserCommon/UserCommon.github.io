95f338fc86fd7b05a6d171456405c15808e8dedf
UserCommon <kirilnenashev@gmail.com>
2023-09-14 23:03:01 +0300

    task 7 finished

---
+++ task7/main.js
@@ -0,0 +1,21 @@
+
+$(document).ready(() => {
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    infinite: true,
+                }
+            },
+
+        ]
+    });
+});
